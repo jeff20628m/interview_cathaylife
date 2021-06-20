fa968294586a5445700a108efba32b4ba9817474
jeffLai <jeff20628m@gmail.com>
2021-06-20 23:21:56 +0800

    fix:EDA script 2021/06/20

--- script/preprocessing.py
+++ script/preprocessing.py
@@ -6,7 +6,6 @@ from sklearn import mixture
 
 
 class preprocessing_training:
-
     def __init__(self, df_train):
 
         self.df = df_train
@@ -24,17 +23,21 @@ class preprocessing_training:
 
         return self.la, self.mapping
 
+    def label_encoding_feature(self, targets: list):
+
+        mapping_list = []
+
+        for target in targets:
+            label_encoding, mapping = self.gen_label_encoder(target)
+            self.df[target] = label_encoding.fit_transform(self.df[target])
+            mapping_list.append(mapping)
+
+        return self.df, mapping_list
+
     def preprocessing(self) -> pd.DataFrame:
 
         # add features
-        # label categories features
 
-        self.la_gender, self.gender_mapping = self.gen_label_encoder('Gender')
-        self.la_vehicle_damage, self.vehicle_damage = self.gen_label_encoder(
-            'Vehicle_Damage')
-        self.df['Gender'] = self.la_gender.transform(self.df['Gender'])
-        self.df['Vehicle_Damage'] = self.la_vehicle_damage.transform(
-            self.df['Vehicle_Damage'])
         self.df = pd.concat(
             [self.df, pd.get_dummies(self.df['Vehicle_Age'])], axis=1)
 
@@ -42,80 +45,119 @@ class preprocessing_training:
         self.df['Age_group'] = pd.cut(self.df['Age'], bins=range(10, 90, 10))
         self.df['Age_group_codes'] = self.df['Age_group'].cat.codes
 
+        # label categories features
+        label_target = ['Gender', 'Vehicle_Damage']
+
+        self.df, mapping_list = self.label_encoding_feature(label_target)
+
         # add interesting_score
         self.df['interesting_score'] = self.df['Vehicle_Damage'] - \
             self.df['Previously_Insured']
 
         # add special_region
         self.df['special_region'] = 0
-        self.special_num = list(
-            self.df[self.df['Response'] == 1]['Region_Code'].value_counts().index[:15])
+        self.special_num = list(self.df[self.df['Response'] == 1]
+                                ['Region_Code'].value_counts().index[:15])
         for region in self.special_num:
             special = self.df[self.df['Region_Code'] == region].index
             self.df.loc[special, 'special_region'] = 1
 
-        # add age * channel
-        self.df['age_channel'] = self.df['Age_group_codes'].astype(
-            'str') + self.df['Policy_Sales_Channel'].astype('str')
-        self.age_channel_la, self.age_channel_mapping = self.gen_label_encoder(
-            'age_channel')
-        self.df['age_channel'] = self.age_channel_la.fit_transform(
-            self.df['age_channel'])
+        # add special_channel
+        self.df['special_Policy_Sales_Channel'] = 0
+        self.special_channel = list(
+            self.df[self.df['Response'] ==
+                    1]['Policy_Sales_Channel'].value_counts().index[:10])
+        for channel in self.special_channel:
+            channel = self.df[self.df['Policy_Sales_Channel'] == channel].index
+            self.df.loc[channel, 'special_Policy_Sales_Channel'] = 1
 
-        # add low & high pay
-        self.df['lowest_pay'] = 0
-        low_idx = self.df[self.df['Annual_Premium']
-                          == self.df['Annual_Premium'].min()].index
-        self.df.loc[low_idx, 'lowest_pay'] = 1
-
-        self.df['high_pay'] = 0
-        high_idx = self.df[self.df['Annual_Premium'] >= (
-            self.df['Annual_Premium'].mean() + 1.5*self.df['Annual_Premium'].std())].index
-        self.df.loc[high_idx, 'high_pay'] = 1
+        self.df['Vehicle_Damage_region'] = self.df['Vehicle_Damage'] + self.df[
+            'special_region']
+        self.df['age_channel'] = self.df['Age_group_codes'] * self.df[
+            'special_Policy_Sales_Channel']
 
         # add annual_std
         self.df['Annual_log_std'] = np.log1p(self.df['Annual_Premium'])
 
-        minmax = MinMaxScaler()
-        different_cluster = minmax.fit_transform(np.array(
-            self.df[['Policy_Sales_Channel', 'Region_Code', 'Age', 'Previously_Insured', 'Vehicle_Damage']]))
-        minmax1 = MinMaxScaler()
-        samiliar_cluster = minmax1.fit_transform(np.array(
-            self.df[['Annual_Premium', 'Gender', 'Vintage', '1-2 Year', '< 1 Year', '> 2 Years']]))
+        # add clustering data
 
-        GMcluster = mixture.GaussianMixture(
-            n_components=2, covariance_type='full', random_state=0)
+        # add diff cluster
+        minmax = MinMaxScaler()
+        different_cluster = minmax.fit_transform(
+            np.array(self.df[[
+                'Policy_Sales_Channel', 'Region_Code', 'Age',
+                'Previously_Insured', 'Vehicle_Damage'
+            ]]))
+
+        GMcluster = mixture.GaussianMixture(n_components=2,
+                                            covariance_type='full',
+                                            random_state=0)
         GMcluster_fit = GMcluster.fit(different_cluster)
         GMlabels_diff = GMcluster_fit.predict(different_cluster)
         self.df['different_cluster'] = GMlabels_diff
 
-        GMcluster = mixture.GaussianMixture(
-            n_components=2, covariance_type='full', random_state=0)
+        # add same cluster
+        minmax1 = MinMaxScaler()
+        samiliar_cluster = minmax1.fit_transform(
+            np.array(self.df[[
+                'Annual_Premium', 'Gender', 'Vintage', '1-2 Year', '< 1 Year',
+                '> 2 Years'
+            ]]))
+        GMcluster = mixture.GaussianMixture(n_components=2,
+                                            covariance_type='full',
+                                            random_state=0)
         GMcluster_fit = GMcluster.fit(samiliar_cluster)
         GMlabels_same = GMcluster_fit.predict(samiliar_cluster)
         self.df['same_cluster'] = GMlabels_same
 
+        # add all cluster
+        minmax2 = MinMaxScaler()
+        all_columns = [
+            'Gender',
+            'Age',
+            'Driving_License',
+            'Region_Code',
+            'Previously_Insured',
+            '1-2 Year',
+            '< 1 Year',
+            '> 2 Years',
+            'Annual_Premium',
+            'Policy_Sales_Channel',
+            'Vintage',
+            'Age_group_codes',
+            'interesting_score',
+            'special_region',
+            'age_channel',
+            'Annual_log_std',
+        ]
+        all_cluster = minmax2.fit_transform(np.array(self.df[all_columns]))
+        GMcluster = mixture.GaussianMixture(n_components=4,
+                                            covariance_type='full',
+                                            random_state=0)
+        GMcluster_fit = GMcluster.fit(all_cluster)
+        GMlabels_all = GMcluster_fit.predict(all_cluster)
+        self.df['all_cluster'] = GMlabels_all
+
         # drop object and category data
         for col in self.df.columns:
-            if (str(self.df[col].dtype) == 'object') | (str(self.df[col].dtype) == 'category'):
+            if (str(self.df[col].dtype) == 'object') | (str(
+                    self.df[col].dtype) == 'category'):
                 print(f'Drop {col} type {self.df[col].dtype}')
                 self.df = self.df.drop(col, axis=1)
 
-        mapping_list = [self.gender_mapping,
-                        self.age_channel_mapping, self.vehicle_damage, self.special_num]
+        mapping_list.append(self.special_num)
+        mapping_list.append(self.special_channel)
 
         return self.df, mapping_list
 
 
 class preprocessing_testing():
-
-    def __init__(self, df_test, map_gender, map_age_channel, map_vehicle_damage, special_num):
-
+    def __init__(self, df_test, mapping_list):
+        """
+        mapping_list label list = 'Gender', 'Vehicle_Damage','special_num','special_channel'
+        """
         self.df = df_test
-        self.map_gender = map_gender
-        self.map_age_channel = map_age_channel
-        self.map_vehicle_damage = map_vehicle_damage
-        self.special_num = special_num
+        self.mapping_list = mapping_list
 
     def preprocessing(self):
 
@@ -123,9 +165,9 @@ class preprocessing_testing():
         # label categories features
 
         self.df['Gender'] = self.df['Gender'].apply(
-            lambda x: self.map_gender[x])
+            lambda x: self.mapping_list[0][x])
         self.df['Vehicle_Damage'] = self.df['Vehicle_Damage'].apply(
-            lambda x: self.map_vehicle_damage[x])
+            lambda x: self.mapping_list[1][x])
 
         self.df = pd.concat(
             [self.df, pd.get_dummies(self.df['Vehicle_Age'])], axis=1)
@@ -140,65 +182,108 @@ class preprocessing_testing():
 
         # add special_region
         self.df['special_region'] = 0
-        for region in self.special_num:
+        for region in self.mapping_list[2]:
             special = self.df[self.df['Region_Code'] == region].index
             self.df.loc[special, 'special_region'] = 1
 
-        # add age * channel
-        self.df['age_channel'] = self.df['Age_group_codes'].astype(
-            'str') + self.df['Policy_Sales_Channel'].astype('str')
-        try:
-            self.df['age_channel'] = self.df['age_channel'].apply(
-                lambda x: self.map_age_channel[x])
-        except KeyError:
-            print("There's non label data!")
-        # it means there's none label data in the mapping list
-        # so we deside to add new label for i and show the problem
-            for key_value in self.df['age_channel']:
-                if key_value not in self.map_age_channel:
-                    # add new label as the max length+1
-                    self.map_age_channel[key_value] = len(
-                        self.map_age_channel) + 1
-                else:
-                    pass
-            self.df['age_channel'] = self.df['age_channel'].apply(
-                lambda x: self.map_age_channel[x])
+        self.df['special_Policy_Sales_Channel'] = 0
+        for channel in self.mapping_list[3]:
+            channel = self.df[self.df['Policy_Sales_Channel'] == channel].index
+            self.df.loc[channel, 'special_Policy_Sales_Channel'] = 1
+
+        self.df['Vehicle_Damage_region'] = self.df['Vehicle_Damage'] + self.df[
+            'special_region']
+        self.df['age_channel'] = self.df['Age_group_codes'] * self.df[
+            'special_Policy_Sales_Channel']
+
+        # for target in label_target:
+        #     try:
+        #         self.df[target] = self.df[target].apply(
+        #             lambda x: self.mapping_list[mapping_code][x])
+        #         mapping_code += 1
+        #     except KeyError:
+        #         print(f"There's non label data in {target}!")
+        #         # it means there's none label data in the mapping list
+        #         # so we deside to add new label for i and show the problem
+        #         for key_value in self.df[target]:
+        #             if key_value not in self.mapping_list[mapping_code]:
+        #                 # add new label as the max length+1
+        #                 self.mapping_list[mapping_code][key_value] = len(
+        #                     self.mapping_list[mapping_code]) + 1
+        #             else:
+        #                 pass
+        #         self.df[target] = self.df[target].apply(
+        #             lambda x: self.mapping_list[mapping_code][x])
+        #         mapping_code += 1
 
         # add low & high pay
-        self.df['lowest_pay'] = 0
-        low_idx = self.df[self.df['Annual_Premium']
-                          == self.df['Annual_Premium'].min()].index
-        self.df.loc[low_idx, 'lowest_pay'] = 1
-
-        self.df['high_pay'] = 0
-        high_idx = self.df[self.df['Annual_Premium'] >= (
-            self.df['Annual_Premium'].mean() + 1.5*self.df['Annual_Premium'].std())].index
-        self.df.loc[high_idx, 'high_pay'] = 1
 
         # add annual_std
         self.df['Annual_log_std'] = np.log1p(self.df['Annual_Premium'])
 
-        minmax = MinMaxScaler()
-        different_cluster = minmax.fit_transform(np.array(
-            self.df[['Policy_Sales_Channel', 'Region_Code', 'Age', 'Previously_Insured', 'Vehicle_Damage']]))
-        minmax1 = MinMaxScaler()
-        samiliar_cluster = minmax1.fit_transform(np.array(
-            self.df[['Annual_Premium', 'Gender', 'Vintage', '1-2 Year', '< 1 Year', '> 2 Years']]))
+        # add clustering data
 
-        GMcluster = mixture.GaussianMixture(
-            n_components=2, covariance_type='full', random_state=0)
+        # add diff cluster
+        minmax = MinMaxScaler()
+        different_cluster = minmax.fit_transform(
+            np.array(self.df[[
+                'Policy_Sales_Channel', 'Region_Code', 'Age',
+                'Previously_Insured', 'Vehicle_Damage'
+            ]]))
+
+        GMcluster = mixture.GaussianMixture(n_components=2,
+                                            covariance_type='full',
+                                            random_state=0)
         GMcluster_fit = GMcluster.fit(different_cluster)
         GMlabels_diff = GMcluster_fit.predict(different_cluster)
         self.df['different_cluster'] = GMlabels_diff
 
-        GMcluster = mixture.GaussianMixture(
-            n_components=2, covariance_type='full', random_state=0)
+        # add same cluster
+        minmax1 = MinMaxScaler()
+        samiliar_cluster = minmax1.fit_transform(
+            np.array(self.df[[
+                'Annual_Premium', 'Gender', 'Vintage', '1-2 Year', '< 1 Year',
+                '> 2 Years'
+            ]]))
+        GMcluster = mixture.GaussianMixture(n_components=2,
+                                            covariance_type='full',
+                                            random_state=0)
         GMcluster_fit = GMcluster.fit(samiliar_cluster)
         GMlabels_same = GMcluster_fit.predict(samiliar_cluster)
         self.df['same_cluster'] = GMlabels_same
 
+        # add all cluster
+        minmax2 = MinMaxScaler()
+        all_columns = [
+            'Gender',
+            'Age',
+            'Driving_License',
+            'Region_Code',
+            'Previously_Insured',
+            '1-2 Year',
+            '< 1 Year',
+            '> 2 Years',
+            'Annual_Premium',
+            'Policy_Sales_Channel',
+            'Vintage',
+            'Age_group_codes',
+            'interesting_score',
+            'special_region',
+            'age_channel',
+            'Annual_log_std',
+        ]
+        all_cluster = minmax2.fit_transform(np.array(self.df[all_columns]))
+        GMcluster = mixture.GaussianMixture(n_components=4,
+                                            covariance_type='full',
+                                            random_state=0)
+        GMcluster_fit = GMcluster.fit(all_cluster)
+        GMlabels_all = GMcluster_fit.predict(all_cluster)
+        self.df['all_cluster'] = GMlabels_all
+
+        # delete the category and object columns
         for col in self.df.columns:
-            if (str(self.df[col].dtype) == 'object') | (str(self.df[col].dtype) == 'category'):
+            if (str(self.df[col].dtype) == 'object') | (str(
+                    self.df[col].dtype) == 'category'):
                 print(f'Drop {col} type {self.df[col].dtype}')
                 self.df = self.df.drop(col, axis=1)
 
